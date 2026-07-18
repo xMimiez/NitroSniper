62b044fd6c185ebe4be2ffc5da1d17b9675c2184
Mime <109782674+xMimiez@users.noreply.github.com>
2026-07-18 18:44:11 -0500

    Update comments in settings.tsx
    Added additional author note in comments.

--- settings.tsx
+++ settings.tsx
@@ -1,6 +1,7 @@
 /*
 Made with ❤️ by neoarz
 Edited with 💜 by N0_.q3
+Edited with ❤️‍🩹 by Vanitywya
 I am not responsible for any damage caused by this plugin; use at your own risk
 Vencord does not endorse/support this plugin (Works with Equicord as well)
 dm @neoarz if u need help or have any questions
